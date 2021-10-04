25d6f1faf40bd253a9ac830bcf7dd0da90674cb9
xio810 <boi4806@naver.com>
2021-10-04 21:39:02 +0900

    숫자 기록 - 기록 전 까지

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState(0);
+
+  const increaseNum = () => {
+    setNum(num + 1);
+  };
+  const decreaseNum = () => {
+    setNum(num - 1 > 0 ? num - 1 : 0);
+  };
+  const cancelNum = () => {
+    setNum(0);
+  };
+  //
+  const addRecordList = () => {};
+  //
   return (
     <div className="App">
-      <div>하하하 안ㄴ여녕 </div>
-      <div>ahahadhfdshfah</div>
-      <div>hahahah</div>
+      <div>현재 숫자 : {num}</div>
+      <div>
+        <button onClick={increaseNum}>증가</button>
+        <button onClick={decreaseNum}>감소</button>
+        <button onClick={cancelNum}>취소</button>
+        <hr />
+        <button onClick={addRecordList}>기록</button>
+        <button>초기화</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
