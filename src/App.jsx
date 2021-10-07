76de08a01f467eea3de645155f3a5a6ccd59df5a
xio810 <boi4806@naver.com>
2021-10-07 19:33:09 +0900

    숫자 기록 - 숫자 삭제 함수로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,110 @@
 import { useState } from "react";
 
-const App = () => {
+const RecordForm = ({ recordedNums, setRecordedNums }) => {
   const [num, setNum] = useState(0);
-  const [recordedNums, setRecordedNums] = useState([3, 6, 8]);
 
-  const increaseNum = () => {
-    setNum(num + 1);
-  };
+  const increaseNum = () => setNum(num + 1);
+
   const decreaseNum = () => {
-    setNum(num - 1 > 0 ? num - 1 : 0);
+    if (num - 1 < 0) return;
+
+    setNum(num - 1);
   };
-  const cancelNum = () => {
-    setNum(0);
-  };
-  //
+
+  const cancelModifyNum = () => setNum(0);
+
   const saveRecord = () => {
     if (num == 0) return;
-    setNum(0);
+
     setRecordedNums([...recordedNums, num]);
+    setNum(0);
   };
 
-  const recordList =
-    recordedNums.length > 0 ? (
-      <div>
-        <h1>RECORD</h1>
-        <ul>
-          {recordedNums.map((num, index) => (
-            <li key={index}>
-              {num}
-              <button onClick={() => delRecordedNum(index)}>삭제</button>
-              <button>수정</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    ) : (
-      <div>없음</div>
-    );
-  //
-  const resetRecord = () => {
+  const clearRecords = () => {
+    setNum(0);
+
+    if (recordedNums.length == 0) return;
+
     setRecordedNums([]);
   };
-  const delRecordedNum = (delIndex) => {
-    setRecordedNums(recordedNums.filter((el, index) => index != delIndex));
-  };
-  //
+
   return (
     <div
       style={{
-        display: "inline-block",
         border: "2px solid black",
         borderRadius: "10px",
         padding: "10px",
       }}
     >
-      <div className="App">
-        <div>현재 숫자 : {num}</div>
-        <div>
-          <button onClick={increaseNum}>증가</button>
-          <button onClick={decreaseNum}>감소</button>
-          <button onClick={cancelNum}>취소</button>
-          <hr />
-          <button onClick={saveRecord}>기록</button>
-          <button onClick={resetRecord}>초기화</button>
-        </div>
-        {recordList}
+      <div>
+        <span>숫자 : {num}</span>
+        <button onClick={increaseNum}>증가</button>{" "}
+        <button onClick={decreaseNum}>감소</button>{" "}
+        <button onClick={cancelModifyNum}>취소</button>
+      </div>
+      <hr />
+      <div>
+        <button onClick={saveRecord}>기록</button>{" "}
+        <button onClick={clearRecords}>초기화</button>
       </div>
     </div>
   );
+};
+
+const RecordList = ({ recordedNums, setRecordedNums }) => {
+  let List = <span>기록없음</span>;
+
+  const delRecordedNum = (delIndex) => {
+    setRecordedNums(recordedNums.filter((el, index) => index != delIndex));
+  };
+
+  if (recordedNums.length > 0) {
+    List = (
+      <ul>
+        {recordedNums.map((recordedNum, index) => (
+          <li key={index}>
+            <span>{recordedNum}</span>
+            &nbsp;
+            <button onClick={() => delRecordedNum(index)}>삭제</button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <div
+      style={{
+        border: "2px solid black",
+        borderRadius: "10px",
+        padding: "10px",
+        marginTop: "10px",
+      }}
+    >
+      {List}
+    </div>
+  );
+};
+
+const RecordApp = () => {
+  const [recordedNums, setRecordedNums] = useState([3, 2, 1]);
+
+  return (
+    <div style={{ display: "inline-block", padding: "10px" }}>
+      <RecordForm
+        recordedNums={recordedNums}
+        setRecordedNums={setRecordedNums}
+      />
+      <RecordList
+        recordedNums={recordedNums}
+        setRecordedNums={setRecordedNums}
+      />
+    </div>
+  );
+};
+
+const App = () => {
+  return <RecordApp />;
 };
 
 export default App;
